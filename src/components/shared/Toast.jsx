3f7e3f0b7dfa54b172304385113d1afd768b8762
lllauren-517 <dora940919@gmail.com>
2026-05-31 14:59:16 +0800

    Delete src directory

--- src/components/shared/Toast.jsx
+++
@@ -1,9 +0,0 @@
-export default function Toast({ toasts }) {
-  return (
-    <div className="toast-area">
-      {toasts.map(t => (
-        <div key={t.id} className={`toast ${t.type}`}>{t.message}</div>
-      ))}
-    </div>
-  )
-}
